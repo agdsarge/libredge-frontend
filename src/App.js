9f7c9ebbb0b0e55212693b5ad68b4254b26dc544
agdsarge <agdsargeant@gmail.com>
2020-07-15 05:09:15 -0400

    fun with grids

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Grid, Image } from 'semantic-ui-react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    render() {
+        return (
+            <Grid style={{backgroundColor: "black"}}>
+                <Grid.Column width={4} >
+                    <div style={{backgroundColor: "lightblue"}}>
+                        <p> GEORGE LUCAS </p>
+                    </div>
+                </Grid.Column>
+                <Grid.Column width={8} >
+                    <div style={{backgroundColor: "limegreen", textAlign: "center"}}>
+                        <p> WES ANDERSON </p>
+                        <p> another p </p>
+                    </div>
+                </Grid.Column>
+                <Grid.Column width={4} >
+                    <div style={{backgroundColor: "coral", textAlign: "right"}}>
+                        <p> DAVID LEAN </p>
+                    </div>
+                </Grid.Column>
+            </Grid>
+            )
+    }
 }
 
-export default App;
+// return (
+//   <div className="App">
+//     <header className="App-header">
+//       <img src={logo} className="App-logo" alt="logo" />
+//       <p>
+//         Edit <code>src/App.js</code> and save to reload.
+//       </p>
+//       <a
+//         className="App-link"
+//         href="https://reactjs.org"
+//         target="_blank"
+//         rel="noopener noreferrer"
+//       >
+//         Learn React
+//       </a>
+//     </header>
+//   </div>
+// );
---
+++ src/redux/reducer.js
@@ -0,0 +1,26 @@
+
+const reducer = (state={}, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
+
+export default reducer
+
+// const initialState = {
+//   count: 50
+// }
+//
+// const reducer = (oldState = initialState, action) => {
+//   switch (action.type) {
+//     case "INCREMENT_COUNTER":
+//       return {count: oldState.count + action.number}
+//     case "DECREMENT_COUNTER":
+//       return {count: oldState.count - action.number}
+//     default:
+//       return oldState
+//   }
+// }
+//
+// export default reducer
